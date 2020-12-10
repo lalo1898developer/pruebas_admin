95ad9ba613c9e73a8e1bd8bb54768546b9cf4dd7
Eduardo Rodriguez Ricardez <lalo.work.tech@outlook.com>
2020-12-10 06:24:42 -0600

    Agregar modelo de User

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,10 +2,12 @@ const { System } = require('./System');
 const { Module } = require('./Module');
 const { Role } = require('./Role');
 const { Profile } = require('./Profile');
+const { User } = require('./User');
 
 module.exports = {
     System,
     Module,
     Role,
     Profile,
+    User,
 };
