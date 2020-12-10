5374e10082456fa9f1b235434f00e68e0af35b77
Eduardo Rodriguez Ricardez <lalo.work.tech@outlook.com>
2020-12-10 06:33:20 -0600

    Agregar controllers para User

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,9 +1,11 @@
 const SystemController = require('./SystemController');
 const ModuleController = require('./ModuleController');
 const RoleController = require('./RoleController');
+const UserController = require('./UserController');
 
 module.exports = {
     SystemController,
     ModuleController,
     RoleController,
+    UserController
 };
