cf7eed490c04c841b32e5f53c05245014a0eaa72
Eduardo Rodriguez Ricardez <lalo.work.tech@outlook.com>
2020-12-10 21:29:46 -0600

    Agregar revisión del idModule para autenticación

--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -3,24 +3,22 @@ const jwt = require('jsonwebtoken');
 module.exports = {
   verifyToken: (req, res, next) => {
     try {
-      const { authorization } = req.headers;
-      const token = authorization.split(' ')[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.decoded = decoded;
-      next();
 
-      /*const { authorization, Module, Permission } = req.headers;
+      const { authorization, module } = req.headers;
       const token = authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.decoded = decoded;
-      decoded.payload.modules.foreach(module => {
-        if(module.Module.toString() == Module)
-          if(module.Permission.toString() == Permission) next();
-      })*/
-      
+      const roles = Object.values(decoded.roles);
+      var is_valid = false;
+
+      roles.forEach(role => {
+        if(role._id == module) is_valid = true;
+      });
+
+      is_valid ? next() : res.status(401).json({ message: 'Auth error' });
+
     } catch (error) {
+      console.log(error);
       res.status(401).json({ message: 'Auth error', error });
     }
   },
 };
-
